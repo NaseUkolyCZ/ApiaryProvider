cf8f163a812245f1f786905a1c27c6ac28aa5f17
davidpodhola <david.podhola@naseukoly.cz>
2015-09-25 21:26:42 +0200

    freshly generated

---
+++ src/ApiaryProvider/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ApiaryProvider")>]
+[<assembly: AssemblyProductAttribute("ApiaryProvider")>]
+[<assembly: AssemblyDescriptionAttribute("Type provider for Apiary.io http://fsprojects.github.io/ApiaryProvider")>]
+[<assembly: AssemblyVersionAttribute("1.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.2")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0.2"
